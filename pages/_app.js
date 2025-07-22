d6f63e8b6e1e9a4ec468cffe402f49805e951de7
gsPatrick <patrickgsiqueira@hotmail.com>
2025-07-22 15:39:07 -0300

    tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,27 @@
 import "@/styles/globals.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Google tag (gtag.js) */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=AW-11168003088"
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-11168003088');
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
